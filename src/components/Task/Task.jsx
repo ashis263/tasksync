a546f0aafdbefcbd9527665c09a1dbfcc6243f93
Ashis Kumar Joy <ashis263@gmail.com>
2025-02-22 16:49:05 +0600

    updated task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { TaskContext } from '../../layouts/MainLayout';
 import moment from 'moment';
 import { useDrag } from 'react-dnd';
+import { useForm } from 'react-hook-form';
+import Modal from 'react-responsive-modal';
 
 const Task = ({ task }) => {
+    const [open, setOpen] = useState(false);
+    const { register, handleSubmit, reset } = useForm();
     const { socket, Toast, user, setTasks, setActivities } = useContext(TaskContext);
     const [{ isDragging }, drag] = useDrag(() => ({
         type: "task",
@@ -34,11 +38,81 @@ const Task = ({ task }) => {
             socket.on('activities', (data) => setActivities(data));
         })
     }
+    const handleEdit = () => {
+        setOpen(true);
+    }
+    const submitForm = async (formData) => {
+        const { deadline: prevDate, ...other } = formData;
+        const data = {
+            ...other,
+            deadline: moment(prevDate).format("MMMM Do YYYY, h:mm A"),
+            oldId: task._id
+        }
+        socket.emit("update", data)
+        const activityData = {
+            operation: `Updated ${task.title}`,
+            title: formData.title,
+            modifiedOn: moment().format("MMMM Do YYYY, h:mm A"),
+            user: user.email
+        }
+        socket.on('updated', () => {
+            socket.emit('modified', activityData);
+            Toast.fire({
+                icon: "success",
+                title: `Updated ${task.title}`
+            });
+            reset();
+            setOpen(false);
+            socket.emit("getTasks", user.email);
+            socket.on("tasks", (tasks) => setTasks(tasks));
+            socket.emit('getActivities', user.email);
+            socket.on('activities', (data) => setActivities(data));
+        })
+    }
     return (
         <div ref={drag} className={`rounded-xl p-5 border shadow-lg shadow-colorOne border-colorOne flex flex-col justify-between text-justify mb-2 h-[50vh] overflow-scroll sm:h-[30vh] ${isDragging ? "opacity-25" : ''}`}>            <h3 className='text-xl font-bold text-colorOne'>{task.title}</h3>
             <p className='overflow-auto'>{task.description}</p>
             <p className='font-mono text-xs my-2'>Deadline:<br />{task.deadline}</p>
-            <button onClick={handleDelete} className='btn-sm btn w-full bg-colorOne text-colorThree hover:bg-colorOne'>Delete</button>
+            <div className='flex justify-between'>
+                <button onClick={handleDelete} className='btn-sm btn bg-colorOne text-colorThree hover:bg-colorOne'>Delete</button>
+                <button onClick={handleEdit} className='btn-sm btn bg-colorOne text-colorThree hover:bg-colorOne'>Update</button>
+            </div>
+            <Modal open={open} onClose={() => setOpen(!open)} center>
+                <h3 className="text-2xl sm:text-4xl sm:px-20 text-center text-colorOne font-medium">Update task</h3>
+                <form onSubmit={handleSubmit(submitForm)}>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className={`label-text`}>Title</span>
+                        </label>
+                        <input type="text" defaultValue={task.title} placeholder="Title" {...register('title', { required: true })} className="input max-lg:input-sm input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className={`label-text`}>Description</span>
+                        </label>
+                        <textarea defaultValue={task.description} className="textarea textarea-bordered" placeholder="Description" {...register('description', { required: true })}></textarea>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className={`label-text`}>Finish Before</span>
+                        </label>
+                        <input defaultValue={task.deadline} type="date" placeholder="Date" {...register('deadline', { required: true })} className="input max-lg:input-sm input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className={`label-text`}>Category</span>
+                        </label>
+                        <select type="checkbox" placeholder="Date" {...register('category', { required: true })} className="input max-lg:input-sm input-bordered" required>
+                            <option selected={task.category === "To Do"}>To Do</option>
+                            <option selected={task.category === "In Progress"}>In Progress</option>
+                            <option selected={task.category === "Done"}>Done</option>
+                        </select>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn max-lg:btn-sm btn-outline text-colorOne lg:text-xl">Update</button>
+                    </div>
+                </form>
+            </Modal>
         </div>
     );
 };
